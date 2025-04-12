f0b44134fbf2f5fd6d28b72bb8856e3e37d53b7a
hy-rf <heyu880202@gmail.com>
2025-04-12 17:44:56 +0800

    chore: add efficiency chart

---
+++ src/components/dashboard/EfficiencyChart.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>Efficiency Chart</h1>
+    <p>This is the efficiency chart component.</p>
+    <div class="chart-container">
+      <!-- Chart will be rendered here -->
+    </div>
+    <p>More details about the efficiency chart can be added here.</p>
+    <p>Additional information or controls can be placed below.</p>
+    <p>End of the efficiency chart component.</p>
+    <p>Additional notes or instructions can be added here.</p>
+  </div>
+</template>
+
+<style lang="css" scoped></style>
