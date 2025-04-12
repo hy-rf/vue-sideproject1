7a0076efe59e86185c9b706d45bc4e444c2a55d7
hy-rf <heyu880202@gmail.com>
2025-04-12 15:55:00 +0800

    chore: add 2 types

---
+++ src/types/Order.ts
@@ -0,0 +1,7 @@
+interface Order {
+  id: number
+  productId: number
+  date: Date
+  weekNum: number
+  quantity: number
+}
---
+++ src/types/Schedule.ts
@@ -0,0 +1,14 @@
+interface Schedule {
+  id: string
+  productName: string
+  operatingLine: string
+  cycleTime: number
+  efficiency: number
+  uph: number
+  workingHoursPerDay: number
+  upd: number
+  minimumWorkingDays: number
+  minimumLotSize: number
+  createdAt: Date
+  notes?: string
+}
