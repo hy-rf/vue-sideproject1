a5cf042eb7710c8cd6ced7b32ee588b9ca8d7e94
hy-rf <heyu880202@gmail.com>
2025-04-12 17:43:53 +0800

    style: remove preset import view style

--- src/views/ImportView.vue
+++ src/views/ImportView.vue
@@ -1,16 +1,8 @@
 <template>
-  <div class="about">
+  <div>
     <h1>import csv file:</h1>
     <label for="avatar">Choose a file<input type="file" /></label>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<style></style>
