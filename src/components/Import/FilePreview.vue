5d1df196d34abda937ea785e0d02a003090f39df
hy-rf <heyu880202@gmail.com>
2025-04-14 16:04:06 +0800

    chore: add file previewer

---
+++ src/components/Import/FilePreview.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>File Preview</h1>
+    <p>This is the file preview component.</p>
+    <div class="preview-container">
+      <!-- File preview will be rendered here -->
+    </div>
+    <p>More details about the file preview can be added here.</p>
+    <p>Additional information or controls can be placed below.</p>
+    <p>End of the file preview component.</p>
+    <p>Additional notes or instructions can be added here.</p>
+    <p>File preview component is designed to show a preview of the uploaded file.</p>
+  </div>
+</template>
+
+<style lang="css" scoped></style>
